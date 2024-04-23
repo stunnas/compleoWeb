79e8d4b19f34b761470078826554d4aee019415b
Chase Albritton <59696546+stunnas@users.noreply.github.com>
2024-04-23 12:42:09 -0400

    text edits

--- compleo-web/src/components/components/typewriterEffect.tsx
+++ compleo-web/src/components/components/typewriterEffect.tsx
@@ -55,7 +55,7 @@ export const TypewriterEffect = ({
                   initial={{}}
                   key={`char-${index}`}
                   className={cn(
-                    `dark:text-white text-black opacity-0 hidden`,
+                    `text-white opacity-0 hidden`,
                     word.className
                   )}
                 >
